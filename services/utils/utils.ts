a03c2159e67ad2c0cb55cf366ea7681e3b12c7dd
Qwikle <qwikle.ziko@gmail.com>
2022-06-07 11:04:33 +0200

    feat search delete and check

--- services/utils/utils.ts
+++ services/utils/utils.ts
@@ -1,7 +1,7 @@
 import Redis from "@ioc:Adonis/Addons/Redis"
 
-export default class Utils {
-    public static makeId(length: number) {
+export default class TokenUtils {
+    private static makeId(length: number) {
         let result = ''
         let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         let charactersLength = characters.length
@@ -12,9 +12,22 @@ export default class Utils {
       }
 
     public static async setToken(email: string) {
-        const token = Utils.makeId(6)
+        const token = TokenUtils.makeId(6)
         await Redis.set(`mobile:verifyEmail:token${email}`, token)
-        await Redis.expire(`mobile:verifyEmail:token${email}`, 1200)
+        await Redis.expire(`mobile:verifyEmail:token${email}`, 43200)
         return token
+    }
+
+    public static searchToken(email: string): Promise<string | null> { 
+        return Redis.get(`mobile:verifyEmail:token${email}`)
+    }
+
+    public static async deleteToken(email: string) {
+        await Redis.del(`mobile:verifyEmail:token${email}`)
+    }
+
+    public static async checkToken(email: string, token: string): Promise<boolean> {
+        const t = await Redis.get(`mobile:verifyEmail:token${email}`)
+        return t === token
     }
 }
