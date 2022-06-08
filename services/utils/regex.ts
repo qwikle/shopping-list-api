7abc192a80c7dc52830f9605b649af6ca63109a4
Qwikle <qwikle.ziko@gmail.com>
2022-06-08 16:59:36 +0200

    refactor change property to method

--- services/utils/regex.ts
+++ services/utils/regex.ts
@@ -1,6 +1,10 @@
 export default class Regex {
-  public static password: RegExp =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  public static password() {
+      return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/
+  }
 
-  public static alphanumeric: RegExp = /^[a-zA-Z0-9]{6}$/
+  public static code() {
+      return /^[a-zA-Z0-9]{6}$/
+  }
+    
 }
