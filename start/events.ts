3d64795fe82ba9601d956259949a5a373048c052
Qwikle <qwikle.ziko@gmail.com>
2022-06-08 12:26:20 +0200

    feat add verifed and reseted password event

--- start/events.ts
+++ start/events.ts
@@ -1,3 +1,5 @@
 import Event from '@ioc:Adonis/Core/Event'
 Event.on('user:forgotPassword', 'User.onForgotPassword')
 Event.on('user:verifyEmail', 'User.onVerifyEmail')
+Event.on('user:verified', 'User.onVerified')
+Event.on('user:resetedPassword', 'User.onResetedPassword')
