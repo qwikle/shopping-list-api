0953b2c3bbb0877f54bc36bb44998806cd926c87
Qwikle <qwikle.ziko@gmail.com>
2022-06-08 12:25:55 +0200

    feat add route to verify email

--- start/routes/user.ts
+++ start/routes/user.ts
@@ -4,4 +4,5 @@ Route.group(() => {
     Route.get('/', 'UserController.index')
     Route.patch('/', 'UserController.update')
     Route.delete('/', 'UserController.delete')
+    Route.post('/verify', 'UserController.verifyEmail');
   }).prefix('user').middleware(['auth'])
