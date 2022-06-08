4a7d017b8f9754aab7b19a3e9340a2a2acf5bd0a
Qwikle <qwikle.ziko@gmail.com>
2022-06-08 12:22:08 +0200

    feat add on reset and and verified events

--- app/Listeners/User.ts
+++ app/Listeners/User.ts
@@ -1,5 +1,7 @@
 import type { EventsList } from '@ioc:Adonis/Core/Event'
 import ForgotPassword from 'App/Mailers/ForgotPassword'
+import ResetedPassword from 'App/Mailers/ResetedPassword'
+import VerifiedEmail from 'App/Mailers/VerifiedEmail'
 
 export default class User {
     public async onForgotPassword(data: EventsList['forgot:password']) {
@@ -16,6 +18,26 @@ export default class User {
         const { user, token } = data
         try {
         const email = new ForgotPassword(user, token)
+        await email.sendLater()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async onVerified(data: EventsList['user:verified']) {
+        const { user } = data
+        try {
+        const email = new VerifiedEmail(user)
+        await email.sendLater()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async onResetedPassword(data: EventsList['user:resetedPassword']) {
+        const { user } = data
+        try {
+        const email = new ResetedPassword(user)
         await email.sendLater()
         } catch (error) {
             console.log(error)
