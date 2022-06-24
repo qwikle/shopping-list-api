da32139f237a973ec43d62d16cb7dc82cfc73429
Qwikle <qwikle.ziko@gmail.com>
2022-06-25 01:02:58 +0200

    refactor change verify- email to get path

--- start/routes.ts
+++ start/routes.ts
@@ -20,11 +20,27 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 import './routes/user'
+// import User from 'App/Models/User'
+// import { DateTime } from 'luxon';
+
 Route.post('/login', 'AuthController.login')
 Route.post('/register', 'AuthController.register')
 Route.post('/forgot-password', 'AuthController.forgotPassword')
 Route.post('/reset-password', 'AuthController.resetPassword')
-Route.post('/verify-email', 'AuthController.verifyEmail')
+Route.get('/verify-email', 'AuthController.verifyEmail').middleware('auth')
 Route.post('/resend-verification-code', 'AuthController.resendVerificationCode')
 Route.post('/logout', 'AuthController.logout')
 Route.post('/check-token', 'AuthController.checkToken')
+
+// Route.get('/', async () => {
+//   const user = User.create({
+//     email: 'qwikle@gmail.com',
+//     password: 'Aazaaz69',
+//   })
+//   ;(await user).related('profile').create({
+//     firstName: 'Qwikle',
+//     lastName: 'Qwikle',
+//     birthDay: DateTime.fromISO('1988-01-01'),
+//     })
+//   return { hello: 'world' }
+// })
